1bcb2b376fb03284b0be1f25cc47089dc6e9851c
JackDevOs <osjack01@gmail.com>
2023-09-26 01:11:26 -0600

    player add

--- Assets/Scripts/MoverJugador.cs
+++ Assets/Scripts/MoverJugador.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class MoverJugador : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+    public float velocidad = 0f;
+    public GameObject obj1;
+    public GameObject obj2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,40 @@ public class MoverJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float mh = Input.GetAxis("Horizontal");
+        float mv = Input.GetAxis("Vertical");
+
+        float s = Input.GetAxis("Jump");
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            s = -10;
+        }
+
+        Vector3 movimiento = new Vector3(mh, s, mv);
+
+        transform.Translate(movimiento * velocidad * Time.deltaTime);
+
         
+        //Objeto Cubo
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Instantiate(obj1, transform.position + transform.forward, Quaternion.identity);
+        }
+
+        //Objeto bala
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            var objBala = Instantiate(obj2, transform.position + transform.forward * 2, Quaternion.identity);
+
+            objBala.GetComponent<moverBala>().Inicializar(transform.forward );
+
+            Destroy(objBala, 10);
+
+        }
+
+       
+
     }
+    
 }
--- Assets/Scripts/moverBala.cs
+++ Assets/Scripts/moverBala.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class moverBala : MonoBehaviour
 {
+    public float velocidad = 10.0f;
+    public Vector3 direccion;
+    GameObject gameObject = new GameObject();
+
+    public void Inicializar(Vector3 direccion)
+    {
+        this.direccion = direccion.normalized;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,13 @@ public class moverBala : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direccion * velocidad * Time.deltaTime, Space.World);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+
 }
